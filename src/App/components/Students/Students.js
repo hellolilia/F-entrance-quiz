a8ea0fe5d4fbc6e319f5ad3a821d0a243e2d28eb
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-27 23:01:14 +0800

    [yyl]feat: can add one student

--- src/App/components/Students/Students.js
+++ src/App/components/Students/Students.js
@@ -6,10 +6,42 @@ class Students extends Component {
     super(props);
     this.state = {
       students: [],
+      newName: '',
     };
   }
 
   componentDidMount() {
+    this.update();
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      newName: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8080/students', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json ',
+        Accept: 'application/json',
+        'Access-Control-Origin': '*',
+      },
+      body: this.state.newName,
+    }).then((response) => {
+      if (response.status === 201) {
+        this.setState({
+          newName: '',
+        });
+        this.update();
+      }
+    });
+  };
+
+  update() {
     fetch('http://localhost:8080/students/list', {
       method: 'GET',
       mode: 'cors',
@@ -27,13 +59,18 @@ class Students extends Component {
       <div>
         <h2>学员列表</h2>
         <ul className="students">
-          {this.state.students.map((item, key) => {
+          {this.state.students.map((item) => {
             return (
               <li key={item} className="student">
-                {key + 1}.{item.name}
+                {item.id}.{item.name}
               </li>
             );
           })}
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="+添加学员" onChange={this.handleChange} />
+            </form>
+          </li>
         </ul>
       </div>
     );
